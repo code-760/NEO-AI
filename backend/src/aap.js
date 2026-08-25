6da8365c717b87404b90efed8572ada97bdb5977
code-760 <himanshu760kumawat@gmail.com>
2026-08-25 11:58:31 +0530

    Add HTML entry point and configure static file serving in the backend
    - Created a new index.html file for the frontend entry point.
    - Updated aap.js to serve static files from the public directory.
    - Added a catch-all route to serve index.html for any unmatched routes.

--- backend/src/aap.js
+++ backend/src/aap.js
@@ -3,9 +3,10 @@ import expsse from 'express';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-
+import path from "path"
 import authRoute from './Routes/auth.route.js';
 import chatRoute from './Routes/chat.route.js';
+import { fileURLToPath } from 'url';
 
 
 
@@ -22,14 +23,25 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
+app.use(expsse.static("./public"))
+
 
 app.get('/', (req, res) => {
   res.json({ message: 'Server is running' });
 });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.use('/auth', authRoute);
 app.use('/chats', chatRoute);
+
+console.log(__dirname)
+app.use("*name",(req,res)=>{
+  res.sendFile(path.join(__dirname,"..","/public/index.html"));
+
+})
 
 
 export default app;
